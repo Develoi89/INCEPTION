15c1ad0eb1ed022244255725b9f0eae9b134d86b
Eloi Alonso <eloi@debian.Eloapelo.com>
2024-05-27 16:57:36 +0200

    wp almost done

---
+++ srcs/requirements/wordpress/tools/wordpress_up.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+sleep 5
+
+if [-f .wp-config.php]
+then
+	echo "founded"
+else
+	wp core download --allow-root
+	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=$HTML_PATH --allow-root
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+       	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
+	wp theme install twentysixteen --activate --allow-root
+	chmod 777 wp-content wp-content/uploads wp-content/uploads/* wp-content/uploads/*/*
+fi
+
+/usr/sbin/php-fpm7.4 -F;
