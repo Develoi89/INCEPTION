2f564163f5cb61b186ab15b634074ee7aa1be02c
ealonso- <eloi@debian.Eloapelo.com>
2024-05-28 19:05:42 +0200

    trying to solve forbiden 403 error

--- srcs/requirements/wordpress/tools/wordpress_up.sh
+++ srcs/requirements/wordpress/tools/wordpress_up.sh
@@ -13,7 +13,7 @@ then
 	echo "founded"
 else
 	wp core download --allow-root
-	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=$HTML_PATH --allow-root
+	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=$/var/www/html --allow-root
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
        	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 	wp theme install twentysixteen --activate --allow-root
