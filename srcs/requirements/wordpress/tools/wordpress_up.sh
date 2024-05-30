00e0fa472736079f4215ea552ac9c041af167015
ealonso- <eloi@debian.Eloapelo.com>
2024-05-30 15:48:18 +0200

    test

--- srcs/requirements/wordpress/tools/wordpress_up.sh
+++ srcs/requirements/wordpress/tools/wordpress_up.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-#esperamos a que el container de MariaDB este up
 
 echo "Waiting Mariadb..."
+
 sleep 16
+
 echo "MariaDB is ready."
 
 if [ -f .wp-config.php ]
@@ -11,9 +12,9 @@ then
 	echo "founded"
 else
 	wp core download --allow-root
-	wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST" --path=/var/www/html --allow-root
-	wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
-       	wp user create "$WP_USER" "$WP_EMAIL" --role=author --user_pass="$WP_PASS" --allow-root
+	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=/var/www/html --allow-root
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+       	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 	wp theme install twentysixteen --activate --allow-root
 	chmod 777 wp-content wp-content/uploads wp-content/uploads/* wp-content/uploads/*/*
 fi
